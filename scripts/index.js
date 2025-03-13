8bf59fc9bded42dbad5252faf238b14bdbf4da39
cattheri <catherinerigunova@yandex.ru>
2025-03-13 19:05:24 +0400

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const menuicon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+const body = document.body;
+const overlay = document.querySelector('.overlay')
+
+menuicon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+    overlay.classList.toggle('overlay');
+});
+
+

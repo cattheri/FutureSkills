a1df2161d61125bd25bcf5016ebccbab744797fc
cattheri <catherinerigunova@yandex.ru>
2025-03-15 04:09:07 +0400

    Add files via upload

---
+++ scripts/comments.js
@@ -0,0 +1,12 @@
+function toggleComments() {
+    let commentsBlock = document.querySelector(".comments-container");
+    commentsBlock.style.display = (commentsBlock.style.display === "none" || commentsBlock.style.display === "") ? "block" : "none";
+}
+function toggleComments2() {
+    let commentsBlock = document.querySelector(".comments-container2");
+    commentsBlock.style.display = (commentsBlock.style.display === "none" || commentsBlock.style.display === "") ? "block" : "none";
+}
+function toggleComments3() {
+    let commentsBlock = document.querySelector(".comments-container3");
+    commentsBlock.style.display = (commentsBlock.style.display === "none" || commentsBlock.style.display === "") ? "block" : "none";
+}
